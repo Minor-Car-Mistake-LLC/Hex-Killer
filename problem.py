81c0865e90ef269ea7b0e0c5e9e2b3fb1328f692
Ikidy <32038178+ikeroy@users.noreply.github.com>
2022-03-29 07:09:27 -0500

    Made the code more "effecient"
    A lil buggy but I'll probably fix it later

--- problem.py
+++ problem.py
@@ -1,20 +1,26 @@
 #read hex data from file
 from dataclasses import replace
 import ffmpeg
+import os
+import random
 
 video = input("Enter the name of the video file (with file extension): ")
 output = input("Enter the name of the output file (without file extension): ")
-
 #check if file extension is .avi
 def check_extension(filename):
-    if filename[-4:] == '.avi':
+    global extension
+    if filename.endswith(".avi"):
+        extension = True
         return True
     else:
+        extension = False
         return False
 
 if check_extension(video) == False:
     #convert video to .avi
-    ffmpeg.input(video).output(output + '.avi').run()
+    ffmpeg.input(video).output(output + 'tmp.avi').run()
+else:
+    extension = True
 
 def read_hex(filename):
     with open(filename, 'rb') as f:
@@ -30,4 +36,25 @@ def write_hex(filename, data):
     with open(filename, 'wb') as f:
         f.write(data)
 
-write_hex(output, replace_char(read_hex(output + '.avi'), b"b7", b"00000000"))
+
+if extension == True:
+    filename = '.avi'
+else:
+    filename = 'tmp.avi'
+
+#get most common character in the file
+def most_common(lst):
+    return max(set(lst), key=lst.count)
+
+#convert the most common character to a byte object
+def most_common_to_byte(most_common_char):
+    return bytes([most_common_char])
+
+
+byte = most_common_to_byte  (most_common(read_hex(video)))
+
+write_hex(output + '.avi', replace_char(read_hex(video), byte, b"00000000"))
+print(byte)
+
+if extension == False:
+    os.remove(output + 'tmp.avi')
